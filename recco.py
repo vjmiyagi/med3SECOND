4d05cda8626dc1dcc67c894a90d45fd1ec9e55f6
vjmiyagi <velmamiyagi@gmail.com>
2020-10-22 14:42:20 -0500

    updating comments

--- recco.py
+++ recco.py
@@ -3,21 +3,22 @@ from sklearn.neighbors import NearestNeighbors
 import pickle
 import spacy
 
-
+# Set up DataFrame
 df = pd.read_csv('csv/med.csv')
 
 
 nlp = spacy.load("en_core_web_sm")
 
-model = "model.pkl"
 
+# Load pickled model
+model = "model.pkl"
 with open(model, 'rb') as file:
    nn = pickle.load(file)
 
 
-# nn = pickle.load("model.pkl")
-
-
+# Define function to recommend strains
+# input is a string consisting of species, effect and flavor
+# returned is 3 recommended strains
 def recommend(input):
    vect = nlp(input).vector
    result = nn.kneighbors(vect.reshape(1,-1))
